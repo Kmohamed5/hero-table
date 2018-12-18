2a0511cc8b4f04ecfa9ae1ecaa92407239943053
Kmohamed5 <44478293+Kmohamed5@users.noreply.github.com>
2018-12-18 15:02:47 -0600

    Update hero-table.js

--- hero-table.js
+++ hero-table.js
@@ -5,49 +5,70 @@ window.onload = function() {
     identity: "Kal-EL",
     powers:["*super speed","*super strength","*Flying","*Ice Breath","*Ect.."],
     weaknesses:["*Kyryptonite","*Not having access to the sun"],
-    yes:"Yes"
+    yes:"Yes",
+    button:"YES HE HAS BOTH CANONICALY AND NON-CANONICALY!",
+    link:"https://en.wikipedia.org/wiki/Superman",
+    buttonnum:5
 }
     spiderman = {
     name: "Spider Man",
     identity: "Peter Parker",
     powers:["*Super Strength","*Spider Sense","*Shoots Webs","*Sticks To Any Surface"],
     weaknesses:"Bullets and anything that kills a normal person",
-    yes: "Yes"
+    yes: "Yes",
+    button:"YES HE HAS BOTH CANONICALY AND NON-CANONICALY!",
+    link:"https://en.wikipedia.org/wiki/Spider-Man",
+    buttonnum:12
 }
     batman = {
     name: "Bat Man",
     identity: "Bruce Wayne",
     powers:["*special weapons","*Money","*Knows every form of martial arts"],
     weaknesses:"Anything that would kill a normal person.",
-    yes: "Yes"
+    yes: "Yes",
+    button:"NO AND YES HE HAS BOTH CANONICALY AND NON-CANONICALY!",
+    link:"https://en.wikipedia.org/wiki/Batman",
+    buttonnum:19
 }
     wonderwoman = {
     name: "Wonder Woman",
     identity: "Diana Prince",
     powers:["*Super strength","*Flight","*Semi-Invulnerable","*Super Speed","*Lasso Of Truth"],
     weaknesses:"Brute force from a god or someone stronger then her",
-    yes: "Yes"
+    yes: "Yes",
+    button:"YES SHES A PRO MURDERING BAD GUYS KINDA LADY!!",
+    link:"https://en.wikipedia.org/wiki/wonder woman",
+    buttonnum:26
 }
     captainamerica = {
     name: "Captain America",
     identity: "Steve Rogers",
     powers:" Anything that a normal person can do but a million times better.",
     weaknesses:"Anything that would kill a normal person.",
-    yes: "No"
+    yes: "No",
+    button:"YES HE WAS A SOLDIER AND WHILE HE DOESNT KILL ANYMORE HE HAS BEFORE!",
+    link:"https://en.wikipedia.org/wiki/captain america",
+    buttonnum:33
 }
     supergirl = {
     name: "Super Girl",
     identity: "Kara Zor-El",
     powers:["*super speed","*super strength","*Flying","*Ice Breath","*Ect.."],
     weaknesses:["*Kyryptonite","*Not having access to the sun"],
-    yes:"Yes"
+    yes:"Yes",
+    button:"YES HE HAS BOTH CANONICALY AND NON-CANONICALY!",
+    link:"https://en.wikipedia.org/wiki/supergirl",
+    buttonnum:40
 }
     greenlantern = {
     name: "Green Lantern",
     identity: "Hal Jordan",
     powers:["*Creates anything they can imagine out of green constructs","*Universal translator","*Flying","*Can breath in space"],
     weaknesses:["*Anything that kills a normal person","*Losing his ring"],
-    yes: "Yes"
+    yes: "Yes",
+    button:"YES HE HAS BOTH CANONICALY AND NON-CANONICALY! (fun fact he in one story killed all of the lanters and technically became a god)",
+    link:"https://en.wikipedia.org/wiki/green lantern",
+    buttonnum: 47
 }
 
 const superHeroArray = [superman,spiderman,batman,wonderwoman,captainamerica,supergirl,greenlantern];
@@ -68,83 +89,27 @@ for (var i=0; i<superHeroArray.length; i++) {
     const yesValue = superHeroArray[i].yes
     const yesId = "yes-"+i;
     document.getElementById(yesId).innerHTML = yesValue
-}
-// 1. Create the button
+    const buttonValue = superHeroArray[i].button
+    const buttonId= "button-"+i;
+    const linkValue = superHeroArray[i].link
+    const linkId= "link-"+i;
+    const buttonnumValue = superHeroArray[i].buttonnum
+    const buttonnumId= "buttonnum-"+i;
 var button = document.createElement("button");
 button.innerHTML = "Click me";
-// 2. Append somewhere
-var dave = document.getElementsByTagName("td")[5];
-var superManById = document.getElementById("superheroone");
+var dave = document.getElementsByTagName('td')[buttonnumValue];
+var superManById = document.getElementById(buttonId);
 dave.appendChild(button);
-// 3. Add event handler
 button.addEventListener ("click", function() {
-  alert("YES HE HAS BOTH CANONICALY AND NON-CANONICALY!");
-});
-// 1. Create the button
-var button = document.createElement("button");
-button.innerHTML = "Click me";
-// 2. Append somewhere
-var dave = document.getElementsByTagName("td")[12];
-var superManById = document.getElementById("superherotwo");
-dave.appendChild(button);
-// 3. Add event handler
-button.addEventListener ("click", function() {
-  alert("YES HE HAS BOTH CANONICALY AND NON-CANONICALY!");
-});
-// 1. Create the button
-var button = document.createElement("button");
-button.innerHTML = "Click me";
-// 2. Append somewhere
-var dave = document.getElementsByTagName("td")[19];
-var superManById = document.getElementById("superherothree");
-dave.appendChild(button);
-// 3. Add event handler
-button.addEventListener ("click", function() {
-  alert("NO AND YES HE HAS BOTH CANONICALY AND NON-CANONICALY!");
-});
-// 1. Create the button
-var button = document.createElement("button");
-button.innerHTML = "Click me";
-// 2. Append somewhere
-var dave = document.getElementsByTagName("td")[26];
-var superManById = document.getElementById("superherofour");
-dave.appendChild(button);
-// 3. Add event handler
-button.addEventListener ("click", function() {
-  alert("YES SHES A PRO MURDERING BAD GUYS KINDA LADY!!");
-});
-// 1. Create the button
-var button = document.createElement("button");
-button.innerHTML = "Click me";
-// 2. Append somewhere
-var dave = document.getElementsByTagName("td")[33];
-var superManById = document.getElementById("superherofive");
-dave.appendChild(button);
-// 3. Add event handler
-button.addEventListener ("click", function() {
-  alert("YES HE WAS A SOLDIER AND WHILE HE DOESNT KILL ANYMORE HE HAS BEFORE!");
-});
-// 1. Create the button
-var button = document.createElement("button");
-button.innerHTML = "Click me";
-// 2. Append somewhere
-var dave = document.getElementsByTagName("td")[40];
-var superManById = document.getElementById("superherosix");
-dave.appendChild(button);
-// 3. Add event handler
-button.addEventListener ("click", function() {
-  alert("YES HE HAS BOTH CANONICALY AND NON-CANONICALY!");
-});
-// 1. Create the button
-var button = document.createElement("button");
-button.innerHTML = "Click me";
-// 2. Append somewhere
-var dave = document.getElementsByTagName("td")[47];
-var superManById = document.getElementById("superheroseven");
-dave.appendChild(button);
-// 3. Add event handler
-button.addEventListener ("click", function() {
-  alert("YES HE HAS BOTH CANONICALY AND NON-CANONICALY! (fun fact he in one story killed all of the lanters and technically became a god)");
+  alert(buttonValue);
 });
 
+document.body.appendChild(button);
+var a = document.createElement('a');
+var linkText = document.createTextNode(nameValue);
+a.appendChild(linkText);
+a.title = nameValue;
+a.href = linkValue;
+document.body.appendChild(a);
+}
 }
